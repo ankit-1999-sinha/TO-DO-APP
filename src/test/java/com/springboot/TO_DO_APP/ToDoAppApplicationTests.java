b959062aad7dcb8e0326da9fa3f84a9030033050
Ankit Sinha <ankitsinha@Ankit-ka-MacBook-Air.local>
2024-10-02 11:09:00 +0530

    my first commit

---
+++ src/test/java/com/springboot/TO_DO_APP/ToDoAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.TO_DO_APP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToDoAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
